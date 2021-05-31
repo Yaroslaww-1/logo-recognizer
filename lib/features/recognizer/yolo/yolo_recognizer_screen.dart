61f851212003db7cb621c279ef58a4d29e79a57b
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-05-31 20:05:02 +0300

    fixed yolo rectangles position

--- lib/features/recognizer/yolo/yolo_recognizer_screen.dart
+++ lib/features/recognizer/yolo/yolo_recognizer_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
+import 'package:logo_recognizer/features/recognizer/camera_config.dart';
 import 'package:logo_recognizer/features/recognizer/yolo/yolo_camera_widget.dart';
 import 'package:logo_recognizer/features/recognizer/recognition.dart';
 import 'package:logo_recognizer/features/recognizer/recognition_widget.dart';
@@ -29,6 +31,41 @@ class _YOLORecognizerScreenState extends State<YOLORecognizerScreen> {
     return _recognitions == null ? [] : _recognitions;
   }
 
+  Recognition updateRecognitionPosition(
+    Recognition recognition,
+  ) {
+    var screenH = CameraConfig.screenSize.height;
+    var screenW = CameraConfig.screenSize.width;
+    var previewH = CameraConfig.inputImageSize.height;
+    var previewW = CameraConfig.inputImageSize.width;
+
+    var _x = recognition.location.left;
+    var _w = recognition.location.width;
+    var _y = recognition.location.top;
+    var _h = recognition.location.height;
+    var scaleW, scaleH, x, y, w, h;
+
+    scaleW = screenH / previewH * previewW;
+    scaleH = screenH;
+    var difW = (scaleW - screenW) / scaleW;
+    x = (_x - difW / 2) * scaleW;
+    w = _w * scaleW;
+    if (_x < difW / 2) w -= (difW / 2 - _x) * scaleW;
+    y = _y * scaleH;
+    h = _h * scaleH;
+
+    return new Recognition(
+      recognition.label,
+      recognition.confidence,
+      new Rect.fromLTWH(
+        x / previewW - 80,
+        y / previewH,
+        w / previewW,
+        h / previewH,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +74,7 @@ class _YOLORecognizerScreenState extends State<YOLORecognizerScreen> {
           YOLOCameraWidget(setRecognitions),
           Stack(
             children: getRecognitions()
+                .map(updateRecognitionPosition)
                 .map(
                   (recognition) => new RecognitionWidget(recognition),
                 )
